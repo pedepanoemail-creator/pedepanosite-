b7a9651ceb9999f92cb7f9cac59909a78df44b61
pedepanoemail-creator <pedepanoemail@gmail.com>
2025-12-31 04:35:47 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '@/components/home/Header';
+import HeroBanner from '@/components/home/HeroBanner';
+import CategoryBar from '@/components/home/CategoryBar';
+import MimosSection from '@/components/home/MimosSection';
+import LancamentosSection from '@/components/home/LancamentosSection';
+import Footer from '@/components/home/Footer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black">
+      <Header />
+      <HeroBanner />
+      <CategoryBar />
+      <MimosSection />
+      <LancamentosSection />
+      <Footer />
+    </div>
+  );
+}
