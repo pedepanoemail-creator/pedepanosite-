3812c60e3e48677d4c9ed1d952d440a7aeeb55c7
pedepanoemail-creator <pedepanoemail@gmail.com>
2026-01-01 11:32:45 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Esta linha garante que o Next.js gere os arquivos no local que a Vercel espera
+  output: 'standalone',
 }
 
 module.exports = nextConfig
