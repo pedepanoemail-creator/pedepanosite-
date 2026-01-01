a02953691f94023cec0e225de8435cedb44d73c0
pedepanoemail-creator <pedepanoemail@gmail.com>
2026-01-01 13:36:16 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black text-white">
-      <div dangerouslySetInnerHTML={{ __html: `
+    <main className="min-h-screen bg-black text-white flex flex-col items-center justify-start p-10">
+      <div 
+        className="w-full max-w-6xl border border-white/20 min-h-[200px]"
+        dangerouslySetInnerHTML={{ __html: `
         
         ` }} />
     </main>
