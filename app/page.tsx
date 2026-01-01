e4475c5796a3e7ea4d74e4a21258237d400fa90e
pedepanoemail-creator <pedepanoemail@gmail.com>
2026-01-01 13:22:15 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import Header from '@/components/home/Header';
-import HeroBanner from '@/components/home/HeroBanner';
-import CategoryBar from '@/components/home/CategoryBar';
-import MimosSection from '@/components/home/MimosSection';
-import LancamentosSection from '@/components/home/LancamentosSection';
-import Footer from '@/components/home/Footer';
-
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black">
-      <Header />
-      <HeroBanner />
-      <CategoryBar />
-      <MimosSection />
-      <LancamentosSection />
-      <Footer />
+    <main className="min-h-screen bg-black text-white">
+      <div dangerouslySetInnerHTML={{ __html: `
+        
+        ` }} />
     </main>
   );
 }
